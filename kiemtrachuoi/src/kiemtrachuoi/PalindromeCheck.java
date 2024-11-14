c9f468ae10e154ea722bb2cc9d84da5c5f9894ab
Thanhcao113 <thanhcao2362005@gmail.com>
2024-11-14 22:16:55 +0700

    assement 8

---
+++ kiemtrachuoi/src/kiemtrachuoi/PalindromeCheck.java
@@ -0,0 +1,14 @@
+package kiemtrachuoi;
+
+public class PalindromeCheck {
+	public static boolean isPalindrome(String str) {
+        String reversed = new StringBuilder(str).reverse().toString();
+        return str.equals(reversed);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("madam")); 
+        System.out.println(isPalindrome("hello")); 
+    }
+}
+
